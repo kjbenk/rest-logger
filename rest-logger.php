76ec625799c25ac7a6a824d5714c3e6936243011
Kyle <kjbenk@gmail.com>
2016-03-22 13:43:41 -0400

    Setup Tests and Continuous Developement

---
+++ rest-logger.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: REST Logger
+ * Plugin URI: https://github.com/kjbenk/rest-logger
+ * Description: A WordPress Plugin that logs WP-API REST Requests.
+ * Author: Kyle Benk
+ * Author URI: https://kylebenk.com
+ * Version: 1.0.0
+ * Text Domain: rlg
+ * Domain Path: languages
+ */
+
+// Exit if accessed directly
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
